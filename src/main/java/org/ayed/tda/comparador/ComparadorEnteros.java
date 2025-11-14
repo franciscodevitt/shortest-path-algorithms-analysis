3b502b399b369b98b0a7a04195d6df207132d35a
Carola Sciaini <csciaini@fi.uba.ar>
2025-11-13 21:50:46 -0300

    se realiza la interfaz de comparador de enteros

---
+++ src/main/java/org/ayed/tda/comparador/ComparadorEnteros.java
@@ -0,0 +1,14 @@
+package org.ayed.tda.comparador;
+
+public class ComparadorEnteros implements Comparador<Integer> {
+
+    @Override
+    public int comparar(Integer a, Integer b) {
+        if (a == null || b == null) {
+            throw new IllegalArgumentException("no se pueden comparar null");
+        }
+        if (a < b) return -1;
+        if (a > b) return 1;
+        return 0;
+    }
+}

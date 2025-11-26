16738f16e92ff2521ee9e7240c4264b3bbdd7244
KuigsDev <axel.julian.gonzalez360@gmail.com>
2025-11-25 23:15:07 -0300

    Implementacion inicial Concesionario y Menu

---
+++ src/main/java/org/ayed/gta/concesionario/Concesionario.java
@@ -0,0 +1,50 @@
+package org.ayed.gta.concesionario;
+
+import org.ayed.gta.Vehiculo;
+import org.ayed.tda.vector.Vector;
+
+public class Concesionario {
+
+    private Vector<Vehiculo> vehiculos;
+
+    private void buscar(String s){
+        String filtro = s.toLowerCase();
+        int max = vehiculos.tamanio();
+        boolean encontrado = false;
+
+        for (int i = 0; i < max; i++) {
+            Vehiculo v = vehiculos.dato(i);
+            if (v.getNombre().toLowerCase().contains(filtro)) {
+                System.out.println(v);
+                encontrado = true;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("No se encontraron vehículos con coincidencias en el nombre/marca.");
+        }
+    }
+
+    public void buscarPorNombre(String nombre) {
+        buscar(nombre);
+    }
+
+    public void buscarPorMarca(String marca) {
+        buscar(marca);
+    }
+
+    public Vehiculo comprarVehiculo(String nombreExacto) {
+        int max = vehiculos.tamanio();
+
+        for (int i = 0; i < max; i++) {
+            Vehiculo v = vehiculos.dato(i);
+            if (v.getNombre().equals(nombreExacto)) {
+                return vehiculos.eliminar(i); // se compra → se elimina del concesionario
+            }
+        }
+
+        throw new IllegalArgumentException("No existe un vehículo con nombre: " + nombreExacto);
+    }
+    
+    
+}

db5bccc7a137287a1a3e15f58b52b967e8329fec
KuigsDev <axel.julian.gonzalez360@gmail.com>
2026-01-27 20:06:53 -0300

    fix del fix

--- src/main/java/org/ayed/gta/main.java
+++ src/main/java/org/ayed/gta/main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 /**
  * Clase simple para probar la interfaz gráfica de misiones.
  */
-public class main {
+public class Main {
 
     public static void main(String[] args) {
         MenuInicio menuInicio = new MenuInicio();
